0caf784b7c62c04f1e871ef2846e6ba93b69d8ca
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2024-11-23 16:35:05 +0100

    chore: changed to poetry

--- bookmarker/settings.py
+++ bookmarker/settings.py
@@ -104,8 +104,6 @@ DATABASES = {
     }
 }
 
-print(DATABASES)
-
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
