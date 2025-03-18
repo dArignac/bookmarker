3926b5c8543197f0c2385d77e8fbaefbec308ed6
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2025-03-18 08:06:08 +0100

    fix: fixes the zone error

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -1,22 +1,13 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getAuth, provideAuth } from '@angular/fire/auth';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    // provideZoneChangeDetection({ eventCoalescing: true }),
+    provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideHttpClient(),
-    // provideFirebaseApp(() => initializeApp(environment.firebase)),
-    // provideAuth(() => {
-    //   const auth = getAuth();
-    //   // FIXME do we need it?
-    //   // connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true });
-    //   return auth;
-    // }),
   ],
 };
