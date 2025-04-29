820c40bd0175a05f826a035689e8324a506b258e
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2025-04-29 13:32:48 +0200

    lists profiles

--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ProfileCreationComponent } from '../profile-creation/profile-creation.component';
 import { SupabaseService } from '../supabase.service';
 import { ToastService } from '../toast.service';
+import { Tables } from '../database.types';
+import { PostgrestError } from '@supabase/supabase-js';
 
 @Component({
   selector: 'app-profiles',
@@ -13,13 +15,14 @@ export class ProfilesComponent implements OnInit {
   sbService = inject(SupabaseService);
   toastService = inject(ToastService);
 
+  profiles: Tables<'profiles'>[] = [];
+
   async ngOnInit(): Promise<void> {
-    // FIXME hwo to type this?
-    const { data, error } = await this.sbService.instance.from('profiles').select();
-    if (error == null) {
-      // FIXME handle
+    const { data, error }: { data: Tables<'profiles'>[] | null; error: PostgrestError | null } = await this.sbService.instance.from('profiles').select();
+    if (error == null && data != null) {
+      this.profiles = data;
     } else {
-      this.toastService.showToast(error.message, 'error');
+      this.toastService.showToast(error!.message, 'error');
     }
   }
 }
