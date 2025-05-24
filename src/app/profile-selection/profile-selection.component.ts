0f067fe706cedad266e4430046f08ac568670951
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2025-05-24 15:54:52 +0200

    profile selection is now fueled by state

--- src/app/profile-selection/profile-selection.component.ts
+++ src/app/profile-selection/profile-selection.component.ts
@@ -1,9 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { PostgrestError, RealtimeChannel } from '@supabase/supabase-js';
-import { SupabaseService } from '../supabase.service';
-import { ToastService } from '../toast.service';
-import { Profile } from '../types';
+import { Component, inject } from '@angular/core';
+import { GLOBAL_RX_STATE } from '../state';
 
 @Component({
   selector: 'app-profile-selection',
@@ -11,56 +8,9 @@ import { Profile } from '../types';
   templateUrl: './profile-selection.component.html',
   styleUrl: './profile-selection.component.scss',
 })
-export class ProfileSelectionComponent implements OnInit, OnDestroy {
-  // FIXME need to use a store to hold the data to decouple and better react on changes and only fetch them once! - using the CDR approach does not update the select
-  sbService = inject(SupabaseService);
-  toastService = inject(ToastService);
-  cdr = inject(ChangeDetectorRef);
-
-  private _profiles: Profile[] = [];
-
-  get profiles(): Profile[] {
-    return this._profiles;
-  }
+export class ProfileSelectionComponent {
+  globalState = inject(GLOBAL_RX_STATE);
+  readonly profiles$ = this.globalState.select('profiles');
 
   selectedProfileId = '';
-
-  profileChangesRealtimeChannel: RealtimeChannel;
-
-  constructor() {
-    this.profileChangesRealtimeChannel = this.sbService.instance
-      .channel('schema-db-changes')
-      .on(
-        'postgres_changes',
-        {
-          schema: 'public', // Subscribes to the "public" schema in Postgres
-          event: '*', // Listen to all changes
-          table: 'profiles', // Listen to profile table only
-        },
-        (payload) => this.loadProfiles()
-      )
-      .subscribe();
-  }
-
-  async ngOnInit(): Promise<void> {
-    //await this.loadProfiles();
-  }
-
-  ngOnDestroy(): void {
-    this.profileChangesRealtimeChannel.unsubscribe();
-  }
-
-  async loadProfiles() {
-    const { data, error }: { data: Profile[] | null; error: PostgrestError | null } = await this.sbService.instance.from('profiles').select('id,name').order('name', { ascending: true });
-    if (error === null) {
-      if (data !== null) {
-        this._profiles = data;
-        this.cdr.detectChanges(); // Trigger change detection
-      } else {
-        this.toastService.showToast('No profile data was returned.', 'info');
-      }
-    } else {
-      this.toastService.showToast(error!.message, 'error');
-    }
-  }
 }
--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -1,10 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, inject, OnDestroy, OnInit } from '@angular/core';
-import { PostgrestError, RealtimeChannel } from '@supabase/supabase-js';
+import { Component, inject } from '@angular/core';
 import { ProfileCreationComponent } from '../profile-creation/profile-creation.component';
-import { SupabaseService } from '../supabase.service';
-import { ToastService } from '../toast.service';
-import { Profile } from '../types';
 import { GLOBAL_RX_STATE } from '../state';
 
 @Component({
@@ -13,60 +9,11 @@ import { GLOBAL_RX_STATE } from '../state';
   templateUrl: './profiles.component.html',
   styleUrl: './profiles.component.scss',
 })
-export class ProfilesComponent implements OnInit, OnDestroy {
-  sbService = inject(SupabaseService);
-  toastService = inject(ToastService);
-  cdr = inject(ChangeDetectorRef);
-
+export class ProfilesComponent {
   globalState = inject(GLOBAL_RX_STATE);
   readonly profiles$ = this.globalState.select('profiles');
 
-  // private _profiles: Profile[] = [];
-
-  // get profiles(): Profile[] {
-  //   return this._profiles;
-  // }
-
   selectedProfileId = '';
-
-  // profileChangesRealtimeChannel: RealtimeChannel;
-
-  // constructor() {
-  // this.profileChangesRealtimeChannel = this.sbService.instance
-  //   .channel('schema-db-changes')
-  //   .on(
-  //     'postgres_changes',
-  //     {
-  //       schema: 'public', // Subscribes to the "public" schema in Postgres
-  //       event: '*', // Listen to all changes
-  //       table: 'profiles', // Listen to profile table only
-  //     },
-  //     (payload) => this.loadProfiles()
-  //   )
-  //   .subscribe();
-  // }
-
-  async ngOnInit(): Promise<void> {
-    //await this.loadProfiles();
-  }
-
-  ngOnDestroy(): void {
-    // this.profileChangesRealtimeChannel.unsubscribe();
-  }
-
-  // async loadProfiles() {
-  //   const { data, error }: { data: Profile[] | null; error: PostgrestError | null } = await this.sbService.instance.from('profiles').select('id,name').order('name', { ascending: true });
-  //   if (error === null) {
-  //     if (data !== null) {
-  //       this._profiles = data;
-  //       this.cdr.detectChanges(); // Trigger change detection
-  //     } else {
-  //       this.toastService.showToast('No profile data was returned.', 'info');
-  //     }
-  //   } else {
-  //     this.toastService.showToast(error!.message, 'error');
-  //   }
-  // }
 
   selectProfileForEdit(profileId: string) {
     this.selectedProfileId = profileId;
