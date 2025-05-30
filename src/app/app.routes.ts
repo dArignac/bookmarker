5861a623a29f63dd4e47a64f7415e2c66a367fd6
Alexander Herrmann <106409+dArignac@users.noreply.github.com>
2025-05-30 09:10:22 +0200

    refactor: moves landing page components to feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { AuthRedirectGuard } from './core/guards/auth-redirect.guard';
 import { AuthGuard } from './core/guards/auth.guard';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorComponent } from './error/error.component';
+import { DashboardComponent } from './features/landing-pages/components/dashboard/dashboard.component';
+import { HomeComponent } from './features/landing-pages/components/home/home.component';
+import { PlaceholderComponent } from './features/landing-pages/components/placeholder/placeholder.component';
 import { ProfilesResolver } from './features/profiles/profiles.resolver';
-import { HomeComponent } from './home/home.component';
 import { LoginRequiredComponent } from './login-required/login-required.component';
 import { LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { TagsComponent } from './tags/tags.component';
 import { UserResolver } from './user.resolver';
 
